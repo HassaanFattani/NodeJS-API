ecc8a5ebc0c9df5b16b8a40a445afb93803e0868
HassaanFattani <hassaan_fattani@hotmail.com>
2019-09-15 00:10:37 +0500

    Change Server Port

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require("http");
 const app = require("./app");
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+app.set("port", process.env.PORT || 3500);
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(app.get("port"), function() {
+  console.log("Server is on port ", app.get("port"));
+});
+//server.listen(port);
