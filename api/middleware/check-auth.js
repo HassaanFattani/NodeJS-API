3ad7fdf2c12b235ebf6e66be7d5d84631906f650
HassaanFattani <hassaan_fattani@hotmail.com>
2019-09-15 00:25:41 +0500

    Changed JWT Key

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -4,7 +4,8 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
 
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    const decoded = jwt.verify(token, "secret");
+    //    const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decoded;
     next();
   } catch (error) {
